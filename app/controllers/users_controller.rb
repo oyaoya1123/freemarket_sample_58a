2f298dc30aab8f058b4366d7d3c9075bcd498fce
yanyu <dcjcsk0205@gmail.com>
2019-11-29 21:28:13 +0900

    ユーザー情報確認1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,4 @@
+class UsersController < ApplicationController
+  def identification  
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'githubtests' => 'githubtests#index'
   get 'githubtests/mypage' => 'githubtests#mypage'
+  get 'users/identification' => 'users#identification'
+  
 end
