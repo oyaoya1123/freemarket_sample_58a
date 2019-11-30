d2ff70d7215bf6fccec4f107daba4a902e573910
Wa-www <0nicoco0@gmail.com>
2019-12-01 00:18:30 +0900

    create_controller
    create products,users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  # マイページ
+  def mypage
+  end
+
+  # ユーザプロフィール編集
+  def profile
+  end
+
+  # 本人情報の登録
+  def identification
+  end
+
+  # クレジットカードの登録
+  def card
+  end
+
+  # ログアウト
+  def logout
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'githubtests' => 'githubtests#index'
-  get 'githubtests/mypage' => 'githubtests#mypage'
-  get 'githubtests/logout' => 'githubtests#logout'
+  # get 'githubtests' => 'githubtests#index'
+  # get 'githubtests/mypage' => 'githubtests#mypage'
+  # get 'githubtests/logout' => 'githubtests#logout'
+
+
+  get 'users/logout' => 'users#logout'
+  get 'users/mypage' => 'users#mypage'
+  get 'users/profile' => 'users#profile'
+  get 'users/identification' => 'users#identification'
+  get 'users/card' => 'users#card'
+  get 'products/index' => 'products#index'
+  get 'products/itemshow' => 'products#itemshow' #仮
+  get 'products/create' => 'products#create'
+  get 'products/buy' => 'products#buy'
+
 end
