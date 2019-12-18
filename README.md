5ecb3ca3c2fa6f768a071fd2ba4413f757f2bccd
kotakosu <11920513k>
2019-12-18 23:38:09 +0900

    商品画像テーブル

--- README.md
+++ README.md
@@ -226,4 +226,3 @@ Things you may want to cover:
 - belongs_to :catergory
 
 
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_12_18_142426) do
+
+  create_table "product_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image_url", null: false
+    t.integer "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
