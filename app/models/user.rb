a0ecefca328a5efefff3df69a2a9be10425a500a
oyaoya1123 <oyaror@yahoo.co.jp>
2019-12-19 00:07:54 +0900

    テストビュー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :users_exhibits, dependent: :destroy
+  has_many :users_purchases, dependent: :destroy
+  has_many :ex_products, through: :users_exhibits, source: :product, dependent: :destroy
+  has_many :pu_products, through: :users_purchases, source: :product, dependent: :destroy
 end
--- app/models/users_exhibit.rb
+++ app/models/users_exhibit.rb
@@ -1,2 +1,4 @@
 class UsersExhibit < ApplicationRecord
+  belongs_to :user
+  belongs_to :product
 end
