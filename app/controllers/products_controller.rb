c50990f3367f4293deab338bf87d2d237e2a045d
Wa-www <0nicoco0@gmail.com>
2019-12-21 23:24:39 +0900

    work_each
    each構文について試行錯誤中

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,22 @@ class ProductsController < ApplicationController
   # 商品一覧
   def index
     @categorys = Category2.all.where(ancestry:nil)
+    # @categorys.each do|parent|
+    #   p[]>>parent
+    #   @category_children1 = Category2.where(ancestry:1)
+    #   @category_children2 = Category2.where(ancestry:794)
+    #   @category_children3 = Category2.where(ancestry:1)
+    #   @category_children4 = Category2.where(ancestry:1)
+    #   @category_children5 = Category2.where(ancestry:1)
+    #   @category_children6 = Category2.where(ancestry:1)
+    #   @category_children7 = Category2.where(ancestry:1)
+    #   @category_children8 = Category2.where(ancestry:1)
+    #   @category_children9 = Category2.where(ancestry:1)
+    #   @category_children10 = Category2.where(ancestry:1)
+    #   @category_children11 = Category2.where(ancestry:1)
+    #   @category_children12 = Category2.where(ancestry:1)
+    #   @category_children13 = Category2.where(ancestry:1)
+    # end
 
   end
 
