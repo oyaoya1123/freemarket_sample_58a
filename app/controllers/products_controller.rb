ef68023ce27bc89e148ec464a3fdbc0860551d71
Wa-www <0nicoco0@gmail.com>
2019-12-21 16:06:02 +0900

    3colomn_exhibit
    商品３カラムの登録実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,14 @@ class ProductsController < ApplicationController
   def index
   end
 
+  def new
+    @product=Product.new
+  end
+
   # 商品出品
   def create
+    @product = Product.create(product_create_params)
+    render :new
   end
 
   # 商品詳細
@@ -13,6 +19,10 @@ class ProductsController < ApplicationController
 
   # 商品購入確認
   def buy
+  end
+
+  def product_create_params
+    params.permit(:name,:description,:price)
   end
   
 end
