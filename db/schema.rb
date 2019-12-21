f2d988f260006cd56baa39eb675520c83b0e6ee8
kotakosu <11920513k>
2019-12-21 23:57:23 +0900

    カード番号テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_21_144240) do
+ActiveRecord::Schema.define(version: 2019_12_21_145044) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_12_21_144240) do
     t.string "city", null: false
     t.string "house_number", null: false
     t.string "building_name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "card_number", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
