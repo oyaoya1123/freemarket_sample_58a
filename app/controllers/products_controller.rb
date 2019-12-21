3fd9b13a09a768994dd18352358a08921a7cb25a
Wa-www <0nicoco0@gmail.com>
2019-12-21 23:56:19 +0900

    top_category_display
    トップページのカテゴリ表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,21 @@
 class ProductsController < ApplicationController
   # 商品一覧
   def index
+    @categorys = Category.all.where(ancestry:nil)
+    @category_children1 = Category.where(ancestry:1)
+    @category_children2 = Category.where(ancestry:199)
+    @category_children3 = Category.where(ancestry:345)
+    @category_children4 = Category.where(ancestry:480)
+    @category_children5 = Category.where(ancestry:622)
+    @category_children6 = Category.where(ancestry:681)
+    @category_children7 = Category.where(ancestry:794)
+    @category_children8 = Category.where(ancestry:894)
+    @category_children9 = Category.where(ancestry:980)
+    @category_children10 = Category.where(ancestry:1089)
+    @category_children11 = Category.where(ancestry:1141)
+    @category_children12 = Category.where(ancestry:1201)
+    @category_children13 = Category.where(ancestry:1264)
+
   end
 
   # 商品出品
