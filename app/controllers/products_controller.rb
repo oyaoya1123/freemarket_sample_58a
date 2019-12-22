f06a85ff25de6aef740ee0f9d5d55be7ad016021
Wa-www <0nicoco0@gmail.com>
2019-12-22 19:39:41 +0900

    modify2
    修正２

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,25 @@
 class ProductsController < ApplicationController
   # 商品一覧
   def index
+    @categories=Category.all
+    # @category=find(id: params[id])
+    # @categories = Category.where(params[:id])
+    # [].each do ||
 
-    @category_children1 = Category.cat1
-    @category_children2 = Category.cat2
-    @category_children3 = Category.cat3
-    @category_children4 = Category.cat4
-    @category_children5 = Category.cat5
-    @category_children6 = Category.cat6
-    @category_children7 = Category.cat7
-    @category_children8 = Category.cat8
-    @category_children9 = Category.cat9
-    @category_children10 = Category.cat10
-    @category_children11 = Category.cat11
-    @category_children12 = Category.cat12
-    @category_children13 = Category.cat13
-
+    # end
+    @category_children1 = Category.cat(1)
+    @category_children2 = Category.cat(2)
+    @category_children3 = Category.cat(3)
+    @category_children4 = Category.cat(4)
+    @category_children5 = Category.cat(5)
+    @category_children6 = Category.cat(6)
+    @category_children7 = Category.cat(7)
+    @category_children8 = Category.cat(8)
+    @category_children9 = Category.cat(9)
+    @category_children10 = Category.cat(10)
+    @category_children11 = Category.cat(11)
+    @category_children12 = Category.cat(12)
+    @category_children13 = Category.cat(13)
   end
 
   # 商品出品
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,23 +1,5 @@
 class Category < ApplicationRecord
   has_ancestry
-  scope :cat1, -> { where(ancestry: 1)}
-  scope :cat2, -> { where(ancestry: 199)}
-  scope :cat3, -> { where(ancestry: 345)}
-  scope :cat4, -> { where(ancestry: 480)}
-  scope :cat5, -> { where(ancestry: 622)}
-  scope :cat6, -> { where(ancestry: 681)}
-  scope :cat7, -> { where(ancestry: 794)}
-  scope :cat8, -> { where(ancestry: 894)}
-  scope :cat9, -> { where(ancestry: 980)}
-  scope :cat10, -> { where(ancestry: 1089)}
-  scope :cat11, -> { where(ancestry: 1141)}
-  scope :cat12, -> { where(ancestry: 1201)}
-  scope :cat13, -> { where(ancestry: 1264)}
 
-
-
-
-
-
-
+  scope :cat, -> (a) {where(ancestry: nil).order(id: :ASC).offset(a)}
 end
