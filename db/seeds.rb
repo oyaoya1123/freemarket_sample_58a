d4d1de4c4da6688f60d1a485c981e88db9624c6c
Wa-www <0nicoco0@gmail.com>
2019-12-24 00:58:17 +0900

    table_seed_create
    その他テーブルの初期値投入用

---
+++ db/seeds.rb
@@ -0,0 +1,46 @@
+Product_conditions.create!(
+  [
+    
+      {name: '新品、未使用'},
+      {name: '未使用に近い'},
+      {name: '目立った傷や汚れなし'},
+      {name: 'やや傷や汚れあり'},
+      {name: '傷や汚れあり'},
+      {name: '全体的に状態が悪い'},
+
+  ]
+) 
+
+Shipping_charges.create!(
+  [
+    
+      {name: '送料込み(出品者負担)'},
+      {name: '着払(購入者負担)'},
+      {name: '目立った傷や汚れなし'},
+      {name: 'やや傷や汚れあり'},
+      {name: '傷や汚れあり'},
+      {name: '全体的に状態が悪い'},
+
+  ]
+) 
+
+Shipping_days.create!(
+  [
+    
+      {name: '1~2日で発送'},
+      {name: '2~3日で発送'},
+      {name: '4~7日で発送'},
+
+  ]
+) 
+
+Produt_statuses.create!(
+  [
+    
+      {name: '出品中'},
+      {name: '取引中'},
+      {name: '売却済み'},
+      {name: '過去の取引'}
+
+  ]
+) 
