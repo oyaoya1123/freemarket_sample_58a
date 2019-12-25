81bd7865105018fd7154e0e82e8be16e46c2f3df
yanyu <dcjcsk0205@gmail.com>
2019-12-25 21:16:37 +0900

    商品出品アクティブハッシュ3

---
+++ db/migrate/20191225120003_create_productconditions.rb
@@ -0,0 +1,9 @@
+class CreateProductconditions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :productconditions do |t|
+      t.integer :prefecture_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_25_062855) do
+ActiveRecord::Schema.define(version: 2019_12_25_120003) do
 
   create_table "activehashes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -72,6 +72,12 @@ ActiveRecord::Schema.define(version: 2019_12_25_062855) do
   create_table "product_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image_url", null: false
     t.integer "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "productconditions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "prefecture_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
