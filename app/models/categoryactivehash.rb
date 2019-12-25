18769d5605d62e5854f3e493ce31f8ed1de58e1a
yanyu <dcjcsk0205@gmail.com>
2019-12-25 20:57:32 +0900

    商品出品アクティブハッシュ2

--- app/models/categoryactivehash.rb
+++ app/models/categoryactivehash.rb
@@ -1,18 +1,18 @@
-class Categoryactivehash < ApplicationRecord
+class Categoryactivehash < ActiveHash::Base
   include ActiveHash::Associations
   self.data = [
       { id: 1, value: 'レディース' },
       { id: 2, value: 'メンズ' },
-      { id: 3, value: '' },
-      { id: 4, value: '着払い(購入者負担)' },
-      { id: 5, value: '着払い(購入者負担)' },
-      { id: 6, value: '着払い(購入者負担)' },
-      { id: 7, value: '着払い(購入者負担)' },
-      { id: 8, value: '着払い(購入者負担)' },
-      { id: 9, value: '着払い(購入者負担)' },
-      { id: 10, value: '着払い(購入者負担)' },
-      { id: 11, value: '着払い(購入者負担)' },
-      { id: 12, value: '着払い(購入者負担)' },
-      { id: 13, value: '着払い(購入者負担)' },
+      { id: 3, value: 'ベビー・キッズ' },
+      { id: 4, value: 'インテリア・住まい・小物' },
+      { id: 5, value: '本・音楽・ゲーム' },
+      { id: 6, value: 'おもちゃ・ホビー・グッズ' },
+      { id: 7, value: 'コスメ・香水・美容' },
+      { id: 8, value: '家電・スマホ・カメラ' },
+      { id: 9, value: 'スポーツ・レジャー' },
+      { id: 10, value: 'ハンドメイド' },
+      { id: 11, value: 'チケット' },
+      { id: 12, value: '自転車・オートバイ' },
+      { id: 13, value: 'その他' }
   ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_25_062518) do
+ActiveRecord::Schema.define(version: 2019_12_25_062855) do
 
   create_table "activehashes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2019_12_25_062518) do
     t.datetime "updated_at", null: false
     t.string "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "categoryactivehashes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "prefecture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "deliverycharges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
