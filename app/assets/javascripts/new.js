426ff670f1c637499bdb854102c2dfe362ea0f77
Wa-www <0nicoco0@gmail.com>
2019-12-29 20:09:16 +0900

    modify
    js_modify

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,6 +1,6 @@
 
 $(function(){
-  var images = [];
+  // var images = [];
   var count =0;
   //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
   $(document).on('change', 'input[type="file"]', function(e) {
@@ -13,17 +13,27 @@ $(function(){
       console.log(file);
       return function(e) {
         // .prevewの領域の中にロードした画像を表示するimageタグを追加
-        $('#preview').append($('<img>').attr({
-                  src: e.target.result,
-                  width: "100px",
-                  class: 'li'+ count,
-                  
-                  title: file.name,
-                  'data-id': count
-              }));
+        // $('#preview').append($('<img>').attr({
+        //           src: e.target.result,
+        //           width: "120px",
+        //           class: 'imagepre'+ count,
+                                    
+        //           title: file.name,
+        //           'data-id': count
+        //       }));
+        var result=e.target.result
+        var btn_wrapper = `<div class="btn_wrapper"><div class="btn edit">編集</div><div class="btn delete">削除</div></div>`;
+        var imagehtml=
+        `<li>
+        <figure class="pre">
+        <img src="${result}" width="100px" class="imagepre" title="${file.name} data-id="${count}">
+        </figure>
+        ${btn_wrapper}
+        </li>`
 
-        var btn_wrapper = $('<div class="btn_wrapper"><div class="btn edit">編集</div><div class="btn delete">削除</div></div>');
-        $(".li"+count).append(btn_wrapper);
+        $('#preview').append(imagehtml);
+
+          // $(this).children().append(btn_wrapper);
 
       };
     })(file);
@@ -36,9 +46,12 @@ $(function(){
       'width', `calc(100% - (20% * ${count}))`
     )
     console.log(count)
+
+    var input_html=`<input class="sell-upload-drop-file" id="upload-image${count}" name="product[product_images_attributes][${count}][image_url]" type="file">`
     // labelタグのfor変える
     $(".label-image").attr('for','upload-image' + count);
-    // inputタグのid変える
-    // $(".sell-upload-drop-file").attr('id','upload-image' + count);
+
+    // inputタグ挿入
+    $("#imageform").append(input_html);
   });
 });
