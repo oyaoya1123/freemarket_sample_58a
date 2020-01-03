4933e0026f299cd924d7cd7473c193af5c01a230
oyaoya1123 <oyaror@yahoo.co.jp>
2020-01-03 16:40:34 +0900

    pluckを利用

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,10 +23,8 @@ class ProductsController < ApplicationController
     @product=Product.new
     @product.product_images.build
 
-    @category_parent_array = ["---"]
-    Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent.name
-    end
+    @category_parent_array = Category.where(ancestry: nil).pluck(:name)
+    @category_parent_array.unshift("---")
 
   end
 
