537fd6cab8cc33b8d7ecc2b2c4f2635590edc2aa
yanyu <dcjcsk0205@gmail.com>
2020-01-09 00:07:48 +0900

    ユーザー関連パンくず作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,9 @@ Rails.application.routes.draw do
   get 'githubtests/mypage' => 'githubtests#mypage'
   get 'users/profile' => 'users#profile'
   get 'users/identification' => 'users#identification'
-  # get 'products/index'  => 'products#index'
-  # get 'products/buy'  => 'products#buy'
-  # get 'products/create'  => 'products#create'
   get 'users/logout' => 'users#logout'
   get 'users/card' => 'users#card'
   get 'users/card_create' => 'users#card_create'
-  # get 'products/itemshow'  => 'products#itemshow'
   get 'users/mypage'  => 'users#mypage'
 
   resource :users do
