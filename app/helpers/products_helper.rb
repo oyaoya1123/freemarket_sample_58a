e7a2fcfafb7d2ffc0a45ac7ceb73d7432388b04e
oyaoya1123 <oyaror@yahoo.co.jp>
2020-01-12 13:18:38 +0900

    to_sの削除

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,10 +3,12 @@ module ProductsHelper
   require 'active_support/all'
 
   def converting_to_jpy(price)
-    "#{price.to_s(:delimited, delimiter: ',')}"
+    #"#{price.to_s(:delimited, delimiter: ',')}"
+    "#{price}"
   end
   
   def expiration_date(month, year)
-    "#{month.to_s}" + " / " + "#{year.to_s.slice(2,3)}"
+    #"#{month.to_s}" + " / " + "#{year.to_s.slice(2,3)}"
+    "#{month}" + " / " + "#{year}"
   end
 end
