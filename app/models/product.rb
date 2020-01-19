c007a71d784d7fa8ee0e63ef27bc208d35c0af1e
Wa-www <0nicoco0@gmail.com>
2020-01-19 15:07:48 +0900

    validate_modify
    バリデーション修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,18 +9,18 @@ class Product < ApplicationRecord
   accepts_nested_attributes_for :product_images, allow_destroy: true
 
   # validates :product_images, presence: true
-  validates :product_images,presence: true, length: { minimum: 1, maximum: 10 }
+  validates :product_images,presence: { message: 'を選択してください。' }, length: { minimum: 1, maximum: 10 }
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true
-  validates :price, presence: true, numericality: {
+  validates :category_id, presence: { message: 'を選択してください。' }
+  validates :price, presence: { message: 'を入力してください。' }, numericality: {
     only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9999999
   }
-  validates :shipping_origin, presence: true
-  validates :shipping_method, presence: true
-  validates :shipping_charge, presence: true
-  validates :shipping_day, presence: true
-  validates :product_condition, presence: true
+  validates :shipping_origin, presence: { message: 'を選択してください。' }
+  validates :shipping_method, presence: { message: 'を選択してください。' }
+  validates :shipping_charge, presence: { message: 'を選択してください。' }
+  validates :shipping_day, presence: { message: 'を選択してください。' }
+  validates :product_condition, presence: { message: 'を選択してください。' }
 
   belongs_to :category
 
