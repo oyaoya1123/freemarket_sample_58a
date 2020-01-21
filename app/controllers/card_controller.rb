533a9650b923e089868b11f86b7784763036c75f
oyaoya1123 <oyaror@yahoo.co.jp>
2020-01-21 23:55:26 +0900

    完了

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -20,4 +20,32 @@ class CardController < ApplicationController
     end
   end
 
+  def pay_create_aside
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    if params['payjp-token'].blank?
+      redirect_to controller: 'users', action: 'card'
+    else
+      customer = Payjp::Customer.create(card: params['payjp-token'])
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to controller: 'users', action: 'show'
+      else
+        redirect_to controller: 'users', action: 'card'
+      end
+    end
+  end
+
+  def pay_delete_aside #PayjpとCardデータベースを削除します
+    card = Card.find_by(user_id: current_user.id)
+    if card.blank?
+      redirect_to controller: 'users', action: 'card'
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+    redirect_to controller: 'users', action: 'show'
+  end
+
 end
